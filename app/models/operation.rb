c7794c0c08a6a1634de1553f6f91817b8dc0c1e6
Borkovskyi-Myroslav <borkovskij.myroslav@gmail.com>
2023-01-28 23:56:36 +0200

    add kaminare and boots..

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,6 +1,8 @@
 class Operation < ApplicationRecord
   belongs_to :category
 
+  paginates_per 2
+
   enum transaction_type: { costs: 0, income: 10 }
 
   validates :amount, presence: true, numericality: { greater_than: 0 }
