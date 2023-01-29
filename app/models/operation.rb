949b921727cec4c3c0958c318cc407dcb1f7d507
Borkovskyi-Myroslav <borkovskij.myroslav@gmail.com>
2023-01-29 17:24:55 +0200

    add foorbar

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -6,6 +6,6 @@ class Operation < ApplicationRecord
   enum transaction_type: { costs: 0, income: 10 }
 
   validates :amount, presence: true, numericality: { greater_than: 0 }
-  validates :odate, :description, presence: true
+  validates :odate, :description,  presence: true
 
 end
