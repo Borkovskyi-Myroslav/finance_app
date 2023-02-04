8c43f8dfdc09f2f35adffde1d4b044b27c4b66ec
Borkovskyi-Myroslav <borkovskij.myroslav@gmail.com>
2023-02-04 22:19:34 +0200

    created the chart and graph

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -21,13 +21,18 @@ class ReportsController < ApplicationController
     @records = @records.where('created_at <= ?', params[:end_date]) if params[:end_date].present?
     @end_date = params[:end_date]
     @start_date = params[:start_date]
+    @trans_type = params[:transaction_type]
 
 
-    @cat_names = Category
-    #@dat_sums = Operation.group("category_id").sum("amount").values
-    @cat_sums = Operation.all
+    # Report by Category
+    @by_cat = @records.group("category_id").sum("amount")
+    @cat_names = @by_cat.map {|a| a[0]}
+    @cat_sums = @by_cat.map {|a| a[1]}
 
-
+    #Report by Date
+    @by_dat = @records.group("strftime(' %d-%m-%Y', odate)").sum("amount")
+    @dat_sum = @by_dat.map {|e| e[1]}
+    @dat_dat = @by_dat.map {|e| e[0]}
 
 
   end
