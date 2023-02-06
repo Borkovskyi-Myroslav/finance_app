5699153912846954d8b3f277684c27b4e2ddc1f5
Borkovskyi-Myroslav <borkovskij.myroslav@gmail.com>
2023-02-06 23:53:53 +0200

    end with graph

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -17,24 +17,14 @@ class ReportsController < ApplicationController
     @records = Operation.all
     @records = @records.where(transaction_type: params[:transaction_type]) if params[:transaction_type].present?
     @records = @records.where(category_id: params[:category_id]) if params[:category_id].present?
-    @records = @records.where('created_at >= ?', params[:start_date]) if params[:start_date].present?
-    @records = @records.where('created_at <= ?', params[:end_date]) if params[:end_date].present?
-    @end_date = params[:end_date]
-    @start_date = params[:start_date]
-    @trans_type = params[:transaction_type]
-    @datus = params[:odate]
-
+    @records = @records.where('odate >= ?', params[:start_date]) if params[:start_date].present?
+    @records = @records.where('odate <= ?', params[:end_date]) if params[:end_date].present?
 
     # Report by Category
-    @by_cat = @records.group("category_id").sum("amount")
-    @cat_names = @by_cat.map {|a| a[0]}
-    @cat_sums = @by_cat.map {|a| a[1]}
+    @by_category = @records.joins(:category).group('categories.name').sum("amount")
 
     #Report by Date
-    @by_dat = @records.group("strftime(' %d-%m-%Y', odate)").sum("amount")
-    @dat_sum = @by_dat.map {|e| e[1]}
-    @dat_dat = @by_dat.map {|e| e[0]}
-
+    @by_date = @records.group("strftime('%d-%m-%Y', odate)").sum("amount")
 
   end
 
