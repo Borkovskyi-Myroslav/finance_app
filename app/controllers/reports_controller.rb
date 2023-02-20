b5aff1c4f365cd492d14acd0c46bcc93d0036bed
Borkovskyi-Myroslav <borkovskij.myroslav@gmail.com>
2023-02-21 00:39:34 +0200

    end test 2

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,14 +6,18 @@ class ReportsController < ApplicationController
   def analytics
 
     @records = Operation.all
+    #тут мені не получилось прописати Scop для транзикації
     @records = @records.where(transaction_type: params[:transaction_type]) if params[:transaction_type].present?
     @records = @records.by_category(params[:category_id])
 
 
-    #@records = @records.by_dates(params[:start_date], params[:end_date])
 
+    # Мені це більше підходить бо тут є 'odate >= ?', воно краще сортує мені під час вибору дат
     @records = @records.where('odate >= ?', params[:start_date]) if params[:start_date].present?
     @records = @records.where('odate <= ?', params[:end_date]) if params[:end_date].present?
+
+    # я можу якось запхати 'odate >= ?' 'odate <= ?' в нижній рядок?
+    #@records = @records.by_dates(params[:start_date], params[:end_date])
 
     # Report by Category
     @by_category = @records.joins(:category).group('categories.name').sum("amount")
